1ebadb2b228f166ef8d7886883af5ab2960cd395
Daniel Bernal <47535947+danielxbernal@users.noreply.github.com>
2020-06-26 22:43:38 -0500

    Add files via upload

---
+++ name.py
@@ -0,0 +1,23 @@
+import time
+
+print ("What is your name?")
+name = input ()
+print ("Nice to meet you " + name)
+
+print ("What do you want?")
+anyanswer = input ()
+print ("Just kidding I don't care haha")
+
+print ("Knock knock...")
+response = input ()
+print ("Self destruct sequence activated")
+time.sleep (1)
+print ("3")
+time.sleep (1)
+print ("2")
+time.sleep (1)
+print ("1")
+time.sleep (1)
+print ("jk :)")
+
+
